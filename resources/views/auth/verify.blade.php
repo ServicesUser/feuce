30d995b76ef17053fbe7be80416eae4a61190d9e
SebastiaN <odm_sd@hotmail.com>
2019-02-14 17:59:20 -0500

    correccion de template de verificacion de email

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,24 +1,35 @@
 @extends('template.usuario')
+@section('titulo') Inicio @endsection
 @section('cuerpo')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+    @component('componentes.usuario.cabecera')
+    @endcomponent
+    <div class="m-grid__item m-grid__item--fluid  m-grid m-grid--ver-desktop m-grid--desktop m-page__container m-body">
+        <div class="m-grid__item m-grid__item--fluid m-wrapper">
+            <div class="m-content">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card">
+                                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                                <div class="card-body">
+                                    @if (session('resent'))
+                                        <div class="alert alert-success" role="alert">
+                                            {{ __('A fresh verification link has been sent to your email address.') }}
+                                        </div>
+                                    @endif
+                                    {{ __('Before proceeding, please check your email for a verification link.') }}
+                                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                                </div>
+                            </div>
                         </div>
-                    @endif
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    @component('componentes.usuario.pie')
+    @endcomponent
 @endsection
 @section('scripts')
     <script>
