fe3835cf19840b4688fc365d8479892803f7e7a9
SebastiaN <odm_sd@hotmail.com>
2019-02-18 14:01:53 -0500

    correccion de inicio de

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,32 @@
 @extends('template.usuario')
 @section('titulo',"Ingresar")
 @section('cuerpo')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    @component('componentes.usuario.cabecera')
+    @endcomponent
+    <div class="m-grid__item m-grid__item--fluid  m-grid m-grid--ver-desktop m-grid--desktop m-page__container m-body">
+        <div class="m-grid__item m-grid__item--fluid m-wrapper">
+            <div class="m-content">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card">
+                                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                                <div class="card-body">
+                                    @if (session('status'))
+                                        <div class="alert alert-success" role="alert">
+                                            {{ session('status') }}
+                                        </div>
+                                    @endif
+                                    You are logged in!
+                                </div>
+                            </div>
                         </div>
-                    @endif
-                    You are logged in!
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    @component('componentes.usuario.pie')
+    @endcomponent
 @endsection
