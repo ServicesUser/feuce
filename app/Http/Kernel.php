0da414aa8d9634e3ca504610c1533f801aafd21a
SebastiaN <odm_sd@hotmail.com>
2019-02-19 18:04:37 -0500

    registro de midlewre

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'adm'   =>  \App\Http\Middleware\Admin::class,
+        'aso'   =>  \App\Http\Middleware\Asociacion::class,
     ];
 
     /**
