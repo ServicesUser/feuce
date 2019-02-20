d1f958a196204d44adb59120c919a25d30bb4bfb
SebastiaN <odm_sd@hotmail.com>
2019-02-20 11:11:31 -0500

    cambio de titulo

--- resources/views/template/invitado.blade.php
+++ resources/views/template/invitado.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>{{config('app.name')}} - @yield('titulo')</title>
+    <title>{{config('app.name')}} | @yield('titulo')</title>
     <meta name="csrf-token" content="{{csrf_token()}}">
     <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
 </head>
