d3344a7d8828cabff1452e5a265d18a52730f005
SebastiaN <odm_sd@hotmail.com>
2019-02-20 11:08:41 -0500

    modelo de movimiento

---
+++ app/Movimiento.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movimiento extends Model
+{
+    protected $primaryKey   =   "id_mo";
+    public $timestamps      =   false;
+}
