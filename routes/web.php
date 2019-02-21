23a8b005f229cd1a9f3630fc43d7f6490a6018fd
SebastiaN <odm_sd@hotmail.com>
2019-02-21 10:20:37 -0500

    rutas para aso

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,14 @@ Route::group(['middleware' => 'verified'], function() {
     Route::get('/home', 'HomeController@index')->name('home');
     Route::prefix('e-voto')->group(function () {
         Route::get('/','MenuController@votosMenu')->name('voto.index');
+
         Route::get('/admin','AdministradorController@formulario')->name('voto.admin');
         Route::post('/admin','AdministradorController@nuevaAso');
         Route::options('/admin','AdministradorController@listaAso');
 
-        Route::get('/aso','AsociacionController@listar');
+
+        Route::get('/aso','AsociacionController@vista')->name('voto.aso');
+        Route::options('/aso','AsociacionController@listar');
     });
 });
 
