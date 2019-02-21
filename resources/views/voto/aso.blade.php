02ba35a906f6b84237073876773cca139d9fc11b
SebastiaN <odm_sd@hotmail.com>
2019-02-21 10:19:50 -0500

    uso de compoenete de campañas

--- resources/views/voto/aso.blade.php
+++ resources/views/voto/aso.blade.php
@@ -6,7 +6,7 @@
     <div class="m-grid__item m-grid__item--fluid  m-grid m-grid--ver-desktop m-grid--desktop m-page__container m-body">
         <div class="m-grid__item m-grid__item--fluid m-wrapper">
             <div class="m-content">
-                <asociaciones></asociaciones>
+                <campanas></campanas>
             </div>
         </div>
     </div>
