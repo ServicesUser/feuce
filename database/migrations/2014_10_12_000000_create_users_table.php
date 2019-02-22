3d0fdf560fd9276602592d31e54bd56c761debd7
SebastiaN <odm_sd@hotmail.com>
2019-02-22 17:40:31 -0500

    clave primaria

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->unsignedInteger('id_ro')->default(3);
             $table->string('name');
             $table->string('details')->nullable();
-            $table->string('email',100)->unique()->nullable();
+            $table->string('email',100)->nullable();
             $table->string('dni')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
