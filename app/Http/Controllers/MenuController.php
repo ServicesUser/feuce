bf5dba83f25b025b7a7a2fcaa7a04a616e2ca08a
SebastiaN <odm_sd@hotmail.com>
2019-02-22 09:51:47 -0500

    subir archivo excel

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use Maatwebsite\Excel\Facades\Excel;
+
+//use Maatwebsite\Excel\Facades\Excel;
 
 class MenuController extends Controller
 {
@@ -57,5 +62,32 @@ class MenuController extends Controller
             'user'  =>  $usuario,
             'notifications' =>  Auth::user()->unreadNotifications
         ]);
+    }
+
+    public function subirExcel(Request $datos){
+        $validacion =   Validator::make($datos->all(),[
+            'excel'   =>  'mimes:xls,xlsx',
+        ]);
+        if ($validacion->fails()) {
+            $mensaje="";
+            foreach ($validacion->errors()->all() as $item)
+                $mensaje.="$item</br>";
+            return (['val'=>false,'mensaje'=>$mensaje,'errores'=>$validacion->errors()->all()]);
+        }else{
+            $archivo        =   $datos->file('excel');
+            $filename       =   strtotime(now()).'.'.$archivo->getClientOriginalExtension();
+            if(Auth::check()) {
+                Storage::disk('local')->deleteDirectory(Auth::user()->id);
+                $save = Storage::disk('local')->putFileAs(Auth::user()->id, $archivo, $filename);
+                $address=storage_path('app/'.Auth::user()->id."/$filename");
+                $resultado=Excel::load($address, function($reader) {
+                    return $reader->get();
+                });
+            }
+            if(@$save)
+                return  (['val'=>true,'mensaje'=>'Se encontraron '.count($resultado->parsed).' cédulas' ,'resultado'=> count($resultado->parsed),'archivo'=>$filename]);
+            else
+                return  (['val'=>false,'mensaje'=>'No se pudo guardar intente de nuevo']);
+        }
     }
 }
