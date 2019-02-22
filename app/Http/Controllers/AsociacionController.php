44707d25e315a568e4b79612eae3dce0ffc65c1e
SebastiaN <odm_sd@hotmail.com>
2019-02-22 09:49:20 -0500

    validacion de nueva eleccion

--- app/Http/Controllers/AsociacionController.php
+++ app/Http/Controllers/AsociacionController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Campana;
-use App\Movimiento;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -24,10 +23,40 @@ class AsociacionController extends Controller
     }
 
     public function nuevaCampania(Request $datos){
+        Validator::extend('base64image', function ($attribute, $value, $parameters, $validator) {
+            $explode = explode(',', $value);
+            $allow = ['png', 'jpg', 'jpeg'];
+            $format = str_replace(
+                [
+                    'data:image/',
+                    ';',
+                    'base64',
+                ],
+                [
+                    '', '', '',
+                ],
+                $explode[0]
+            );
+            if (!in_array($format, $allow))
+                return false;
+            if (!preg_match('%^[a-zA-Z0-9/+]*={0,2}$%', $explode[1]))
+                return false;
+            return true;
+        });
+
         $validacion =   Validator::make($datos->all(),[
-            'detalle'   =>  'nullable|string|max:500',
-            'desde'     =>  'required|date',
-            'hasta'     =>  'required|date|after:desde',
+            'detalle'               =>  'nullable|string|max:500',
+            'fecha'                 =>  'required|date',
+            'inicio'                =>  'required|date',
+            'fin'                   =>  'required|date|after:inicio',
+            'excel.cantidad'        =>  'required|numeric|min:1',
+            'miembros'              =>  'required',
+            'miembros.*.dignidad'   =>  'required|exists:tipo_candidato,id_tc',
+            'miembros.*.movimiento' =>  'required|exists:movimientos,id_mo',
+            'miembros.*.nombre'     =>  'required|max:250',
+            'miembros.*.foto'       =>  'required|base64image',
+        ],[
+            'miembros.*.foto.base64image'   =>  "Suba un archivo de imagen válido"
         ]);
         if ($validacion->fails()) {
             $mensaje="";
@@ -41,25 +70,8 @@ class AsociacionController extends Controller
             $campana->desde_ca      =   $datos->desde;
             $campana->vence_ca      =   $datos->hasta;
             $campana->id_us         =   Auth::user()->id;
-            $campana->id_ca         =   hash('fnv1a32 ',$numero.Auth::user()->id);
+            $campana->id_ca         =   hash('fnv1a32',$numero.Auth::user()->id);
             $campana->save();
-            return (['val'=>true,'mensaje'=>'Se ha guardado correctamente']);
-        }
-    }
-
-    public function nuevoMovimiento(Request $datos){
-        $validacion =   Validator::make($datos->all(),[
-            'nombre'    =>  'required|string'
-        ]);
-        if ($validacion->fails()) {
-            $mensaje="";
-            foreach ($validacion->errors()->all() as $item)
-                $mensaje.="$item</br>";
-            return (['val'=>false,'mensaje'=>$mensaje,'errores'=>$validacion->errors()->all()]);
-        }else{
-            $movimiento             =   new Movimiento();
-            $movimiento->titulo_mo  =   $datos->nombre;
-            $movimiento->save();
             return (['val'=>true,'mensaje'=>'Se ha guardado correctamente']);
         }
     }
