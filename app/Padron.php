4f93f6c08f27b12c6c3c98a6e6c72c58294f9886
SebastiaN <odm_sd@hotmail.com>
2019-02-26 08:56:32 -0500

    padron electoral

---
+++ app/Padron.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Padron extends Model
+{
+    protected $table        =       "padrones";
+    public $timestamps      =       true;
+}
