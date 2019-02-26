1608584cacab4b2dfdb01c3222af7acd4b7e1b85
SebastiaN <odm_sd@hotmail.com>
2019-02-26 08:57:51 -0500

    ruta ejemplo

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -37,4 +40,9 @@ Route::group(['middleware' => 'verified'], function() {
 Route::prefix('app')->group(function () {
     Route::get('/','MenuController@generado');
     Route::post('/archivo','MenuController@subirExcel');
+
+    Route::get('/prueba',function(){
+        $var=\App\Campana::find('88e38716');
+        return response()->json($var->padron);
+    });
 });
