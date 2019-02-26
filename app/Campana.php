ce263cb9fcfd73a245a61174c7c3a809fbc3ff5f
SebastiaN <odm_sd@hotmail.com>
2019-02-26 08:54:19 -0500

    serializacion de fechas en la tabla campaña

--- app/Campana.php
+++ app/Campana.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Date\Date;
 
 class Campana extends Model
 {
@@ -13,4 +14,24 @@ class Campana extends Model
     protected $hidden           =       ['id_us'];
 
     protected $casts            =       ['estado_ca'=>'bool'];
+
+    protected $appends          =       ['dia','desde','hasta'];
+
+
+    public function getDiaAttribute(){
+        return Date::createFromFormat('Y-m-d H:i:s',$this->attributes['desde_ca'])->format('l, d \\d\\e F \\d\\e\\l Y');
+    }
+
+    public function getDesdeAttribute(){
+        return Date::createFromFormat('Y-m-d H:i:s',$this->attributes['desde_ca'])->toTimeString();
+    }
+    public function getHastaAttribute(){
+        return Date::createFromFormat('Y-m-d H:i:s',$this->attributes['vence_ca'])->toTimeString();
+    }
+
+    public function padron(){
+        return $this->hasMany(Padron::class,'id_ca','id_ca');
+    }
+
+
 }
